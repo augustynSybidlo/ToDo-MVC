25952bc53ccc511c74d0f56598eca22a491a1737
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2017-11-04 18:26:47 +0100

    Module: todo_item - instance methods created, module complete

--- todo_item.py
+++ todo_item.py
@@ -4,11 +4,11 @@ class ToDo(object):
         if isinstance(name, str):
             self.name = name
         else:
-            raise ValueError("Name must be a string")
+            raise ValueError("Name must be a string!")
         if isinstance(description, str):
             self.description = description
         else:
-            raise ValueError("Description must be a string")
+            raise ValueError("Description must be a string!")
         self.is_done = False
 
     def mark(self):
@@ -21,13 +21,13 @@ class ToDo(object):
         if isinstance(new_name, str):
             self.name = new_name
         else:
-            raise ValueError("Name must be a string")
+            raise ValueError("Name must be a string!")
 
     def change_description(self, new_description):
         if isinstance(new_description, str):
             self.description = new_description
         else:
-            raise ValueError("Description must be a string")
+            raise ValueError("Description must be a string!")
 
     def __str__(self):
         if self.is_done is True:
