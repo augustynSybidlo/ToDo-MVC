b6743cd59dd1a8645f324d6b4e4fb1110ec36a23
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2017-11-04 18:45:47 +0100

    Module:todo_container-new instance method created (add_todo_item)

--- todo_container.py
+++ todo_container.py
@@ -4,4 +4,7 @@ from todo_item import ToDo
 class ToDoContainer(object):
 
     def __init__(self):
-        self.notes = []
+        self.todo_items = []
+
+    def add_todo_item(self):
+        self.todo_items.append(ToDo(name, description))
